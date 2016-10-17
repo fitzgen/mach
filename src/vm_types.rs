2a98f715d10e0ae75e9164309e3de360a2aae783
Elliott Linder <elliott.darfink@gmail.com>
2016-10-17 12:20:15 +0200

    Add additional definitions to vm_types

--- src/vm_types.rs
+++ src/vm_types.rs
@@ -1,6 +1,7 @@
 //! This module roughly corresponds to `mach/i386/vm_types.h`.
 
 pub type natural_t = ::libc::c_uint;
+pub type integer_t = ::libc::c_int;
 
 pub type user_addr_t = usize;
 
@@ -10,5 +11,7 @@ pub type mach_vm_size_t    = u64;
 pub type vm_map_offset_t   = u64;
 pub type vm_map_address_t  = u64;
 pub type vm_map_size_t     = u64;
+pub type vm_offset_t       = ::libc::uintptr_t;
+pub type vm_size_t         = ::libc::uintptr_t;
 
 pub type mach_port_context_t = mach_vm_address_t;
