30120817f5ddec9fb53ac1b0b44ec8460a836731
Holden Stegman <holden@azuqua.com>
2017-05-18 12:30:33 -0700

    Add time_value.h helper functions for manipulating time_value structs

--- src/time_value.rs
+++ src/time_value.rs
@@ -2,6 +2,8 @@
 
 use vm_types::{integer_t};
 
+const TIME_MICROS_MAX: integer_t = 1000000;
+
 #[derive(Debug)]
 #[repr(C)]
 #[repr(packed)]
@@ -17,5 +19,62 @@ impl Default for time_value_t {
       seconds: 0,
       microseconds: 0
     }
+  }
+}
+
+pub fn time_value_add_usec(val: &mut time_value_t, micros: integer_t) {
+  val.microseconds = val.microseconds + micros;
+  if val.microseconds >= TIME_MICROS_MAX {
+    val.microseconds = val.microseconds - TIME_MICROS_MAX;
+    val.seconds = val.seconds + 1;
+  }
+}
+
+pub fn time_value_add(result: &mut time_value_t, addend: &time_value_t) {
+  result.microseconds = result.microseconds + addend.microseconds;
+  result.seconds = result.seconds + addend.seconds;
+  if result.microseconds >= TIME_MICROS_MAX {
+    result.microseconds = result.microseconds - TIME_MICROS_MAX;
+    result.seconds = result.seconds + 1;
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use time_value::{
+    time_value,
+    time_value_t,
+    time_value_add_usec,
+    time_value_add
+  };
+
+  #[test]
+  fn check_time_value_add_usec() {
+    let mut t_val = time_value::default();
+    time_value_add_usec(&mut t_val, 900);
+    assert!(t_val.seconds == 0 && t_val.microseconds == 900);
+  }
+
+  #[test]
+  fn check_time_value_add_usec_over() {
+    let mut t_val = time_value::default();
+    time_value_add_usec(&mut t_val, 1000001);
+    assert!(t_val.seconds == 1 && t_val.microseconds == 1);
+  }
+
+  #[test]
+  fn check_time_value_add() {
+    let mut t_val = time_value::default();
+    let t_val_other = time_value { seconds: 3, microseconds: 100 };
+    time_value_add(&mut t_val, &t_val_other);
+    assert!(t_val.seconds == 3 && t_val.microseconds == 100)
+  }
+
+  #[test]
+  fn check_time_value_add_over() {
+    let mut t_val = time_value { seconds: 3, microseconds: 999900 };
+    let t_val_other = time_value { seconds: 0, microseconds: 100 };
+    time_value_add(&mut t_val, &t_val_other);
+    assert!(t_val.seconds == 4 && t_val.microseconds == 0)
   }
 }
