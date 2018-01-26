accdcb512d696b7f2e11091062ae1514b91f4c05
Julia Evans <julia@jvns.ca>
2018-01-25 23:18:42 -0500

    Add task_info definition to task module

--- src/task.rs
+++ src/task.rs
@@ -1,8 +1,10 @@
 //! This module corresponds to `mach/task.defs`.
 
 use kern_return::{kern_return_t};
+use mach_types::task_name_t;
 use message::{mach_msg_type_number_t};
 use port::{mach_port_t};
+use task_info::{task_flavor_t, task_info_t};
 use types::{task_t, thread_act_array_t};
 
 pub type task_special_port_t = ::std::os::raw::c_int;
@@ -21,4 +23,14 @@ extern "C" {
     pub fn task_threads(target_task: task_t,
                         act_list: *mut thread_act_array_t,
                         act_list_cnt: *mut mach_msg_type_number_t) -> kern_return_t;
+    pub fn task_info(
+        target_task: task_name_t,
+        flavor: task_flavor_t,
+        task_info_out: task_info_t,
+        task_info_outCnt: *mut mach_msg_type_number_t) -> kern_return_t;
+    pub fn task_set_info(
+        target_task: task_t,
+        flavor: task_flavor_t,
+        task_info_in: task_info_t,
+        task_info_inCnt: mach_msg_type_number_t) -> kern_return_t;
 }
